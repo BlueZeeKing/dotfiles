933239468c39ce52ac7da99c303e3d120c127149
BlueZeeKing <github@blueish.dev>
2023-07-19 23:06:02 -0400

    Add diagnostics binding

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,9 +2,9 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>tc', builtin.commands, {})
 vim.keymap.set('n', '<leader>sc', builtin.spell_suggest, {})
 vim.keymap.set('n', '<leader>ss', builtin.lsp_dynamic_workspace_symbols, {})
+vim.keymap.set('n', '<leader>d', builtin.diagnostics, {})
 
 -- require('telescope').setup {
 -- 	extensions = {
