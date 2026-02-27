051c311f1a412540508e6e3267593168571aae99
Brayden Zee <github@blueish.dev>
2026-02-27 10:47:50 -0500

    Move things so files are no longer hidden and add foot config

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env zsh
 
-stow alacritty
-stow atuin
-stow gh
-stow lazygit
-stow nvim
-stow scripts
-stow starship
-stow tmux
-stow zsh
-stow dunst
-stow hypr
-stow waybar
-stow wofi
-stow wlogout
+stow --dotfiles alacritty
+stow --dotfiles atuin
+stow --dotfiles gh
+stow --dotfiles lazygit
+stow --dotfiles nvim
+stow --dotfiles scripts
+stow --dotfiles starship
+stow --dotfiles tmux
+stow --dotfiles zsh
+stow --dotfiles dunst
+stow --dotfiles hypr
+stow --dotfiles waybar
+stow --dotfiles wofi
+stow --dotfiles wlogout
+stow --dotfiles foot
 
 mkdir ~/zsh-plugins
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/zsh-plugins/zsh-autosuggestions
---
+++ nvim/dot-config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,62 @@
+return {
+	-- {
+	-- 	"stevearc/conform.nvim",
+	-- 	config = function()
+	-- 		require("conform").setup({
+	-- 			format_on_save = {
+	-- 				timeout_ms = 10000,
+	-- 				lsp_fallback = false,
+	-- 			},
+	-- 			formatters_by_ft = {
+	-- 				lua = { "stylua" },
+	-- 				rust = { "rustfmt", "rustywind", "leptosfmt" },
+	-- 				toml = { "taplo" },
+	-- 				javascript = { "biome" },
+	-- 				javascriptreact = { "biome" },
+	-- 				typescript = { "biome" },
+	-- 				typescriptreact = { "biome" },
+	-- 				vue = { "prettier" },
+	-- 				css = { "prettier" },
+	-- 				scss = { "prettier" },
+	-- 				less = { "prettier" },
+	-- 				html = { "biome" },
+	-- 				htmldjango = { "biome" },
+	-- 				-- json = { "biome" },
+	-- 				-- jsonc = { "prettier" },
+	-- 				yaml = { "prettier" },
+	-- 				["markdown.mdx"] = { "prettier" },
+	-- 				graphql = { "prettier" },
+	-- 				handlebars = { "prettier" },
+	-- 				svelte = { "prettier" },
+	-- 				-- java = { "google-java-format" },
+	-- 				haskell = { "ormolu" },
+	-- 				elixir = { "mix" },
+	-- 				-- vala = { "uncrustify" },
+	-- 				-- c = { "clang-format" },
+	-- 				cpp = { "clang-format" },
+	-- 			},
+	-- 			formatters = {
+	-- 				["google-java-format"] = {
+	-- 					args = { "-" },
+	-- 				},
+	-- 				leptosfmt = {
+	-- 					command = "leptosfmt",
+	-- 					args = { "--stdin" },
+	-- 				},
+	-- 				rustfmt = {
+	-- 					args = { "--emit=stdout", "--edition=2021" },
+	-- 				},
+	-- 				rustywind = {
+	-- 					require_cwd = true,
+	-- 					cwd = require("conform.util").root_file({
+	-- 						"tailwind.config.js",
+	-- 						"tailwind.config.cjs",
+	-- 						"tailwind.config.mjs",
+	-- 						"tailwind.config.ts",
+	-- 					}),
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
+}
